3ead1a7f5159ed3156a1530fab1dd1f18cf066e9
S0N_0F_A_S0N <S0N-0F-A-S0N@users.noreply.github.com>
2018-01-01 16:19:07 -0600

    TJMVCEZ

---
+++ src/TechJobs/Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TechJobs.Models;
+
+namespace TechJobs.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
+            ViewBag.searchTerm = "";
+            return View();
+        }
+
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.searchTerm = searchTerm;
+
+            if (searchType.Equals("all")) {
+                ViewBag.jobs = JobData.FindByValue(searchTerm);
+            } else {
+                ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ src/TechJobs/Models/JobData.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace TechJobs.Models
+{
+    class JobData
+    {
+        static List<Dictionary<string, string>> AllJobs = new List<Dictionary<string, string>>();
+        static bool IsDataLoaded = false;
+
+        public static List<Dictionary<string, string>> FindAll()
+{
+            LoadData();
+            return new List<Dictionary<string, string>>(AllJobs);
+        }
+
+        public static List<string> FindAll(string column)
+        {
+            LoadData();
+
+            List<string> values = new List<string>();
+            foreach (Dictionary<string, string> job in AllJobs) {
+                string aValue = job[column];
+
+                if (!values.Contains(aValue)) {
+                    values.Add(aValue);
+                }
+            }
+            values.Sort();
+            return values;
+        }
+
+        public static List<Dictionary<string, string>> FindByValue(string value)
+        {
+            LoadData();
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+            foreach (Dictionary<string, string> row in AllJobs) {
+                foreach (string key in row.Keys) {
+                    string aValue = row[key];
+                    if (aValue.ToLower().Contains(value.ToLower())) {
+                        jobs.Add(row);
+                        break;
+                    }
+                }
+            }
+
+            return jobs;
+        }
+
+        public static List<Dictionary<string, string>> FindByColumnAndValue(string column, string value)
+        {
+            LoadData();
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+
+            if (value.Length == 0) return jobs;
+            foreach (Dictionary<string, string> row in AllJobs) {
+                if (row.ContainsKey(column)) {
+                    string aValue = row[column];
+                    if (aValue.ToLower().Contains(value.ToLower())) {
+                        jobs.Add(row);
+                    }
+                }
+            }
+            return jobs;
+        }
+
+        private static void LoadData()
+        {
+
+            if (IsDataLoaded) return;
+
+            List<string[]> rows = new List<string[]>();
+            using (StreamReader reader = File.OpenText("Models/job_data.csv"))
+            {
+                while (reader.Peek() >= 0) {
+                    string line = reader.ReadLine();
+                    string[] rowArrray = CSVRowToStringArray(line);
+                    if (rowArrray.Length > 0) {
+                        rows.Add(rowArrray);
+                    }
+                }
+            }
+
+            string[] headers = rows[0];
+            rows.Remove(headers);
+            foreach (string[] row in rows)   {
+                Dictionary<string, string> rowDict = new Dictionary<string, string>();
+                for (int i = 0; i < headers.Length; i++) {
+                    rowDict.Add(ToTitle(headers[i]), row[i]);
+                }
+                AllJobs.Add(rowDict);
+            }
+
+            IsDataLoaded = true;
+        }
+
+        private static string ToTitle(string s)
+        {
+            return new System.Globalization.CultureInfo("en-US", false).TextInfo.ToTitleCase(s.ToLower());
+        }
+
+        private static string[] CSVRowToStringArray(string row, char fieldSeparator = ',', char stringSeparator = '\"')
+        {
+            bool isBetweenQuotes = false;
+            StringBuilder valueBuilder = new StringBuilder();
+            List<string> rowValues = new List<string>();
+            foreach (char c in row.ToCharArray()) {
+                if ((c == fieldSeparator && !isBetweenQuotes)) {
+                    rowValues.Add(valueBuilder.ToString());
+                    valueBuilder.Clear();
+                } else {
+                    if (c == stringSeparator){
+                        isBetweenQuotes = !isBetweenQuotes;
+                    } else {
+                        valueBuilder.Append(c);
+                    }
+                }
+            }
+
+            rowValues.Add(valueBuilder.ToString());
+            valueBuilder.Clear();
+            return rowValues.ToArray();
+        }
+    }
+}
